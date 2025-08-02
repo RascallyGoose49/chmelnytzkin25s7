6516bf3b9925b162f50b44b08ba892399074fcba
RascallyGoose49 <noah.j.chmelnytzki@gmail.com>
2025-08-01 22:03:57 -0400

    Comments and collision detection
    Added necessary comments and collision detection in which the balls change colour

--- assignment4/part3/main.js
+++ assignment4/part3/main.js
@@ -18,6 +18,9 @@ function randomRGB() {
     return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
+
+/* Classifies the balls and their properties */
+
 class Ball {
     constructor(x, y, velX, velY, color, size) {
         this.x = x;
@@ -26,12 +29,19 @@ class Ball {
         this.velY = velY;
         this.color = color;
         this.size = size;
+
+
+        /* Draws the ball */
+        
     } draw() {
         ctx.beginPath();
         ctx.fillStyle = this.color;
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
         ctx.fill();
     }
+
+    /* Updates the data */
+
     update() {
         if (this.x + this.size >= width) {
             this.velX = -this.velX;
@@ -52,9 +62,28 @@ class Ball {
         this.x += this.velX;
         this.y += this.velY;
     }
+
+    /* Collision detection */
+
+    collisionDetect() {
+        for (const ball of balls) {
+            if (this !== ball) {
+                const dx = this.x - ball.x;
+                const dy = this.y - ball.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + ball.size) {
+                    ball.color = this.color = randomRGB();
+                }
+            }
+        }
+    }
 }
 
 const balls = [];
+
+
+/* Animates the balls */
 
 while (balls.length < 25) {
     const size = random(10, 20);
@@ -72,16 +101,21 @@ while (balls.length < 25) {
     balls.push(ball);
 }
 
+
+/* Loops the balls and background colour */
+
 function loop() {
-  ctx.fillStyle = "rgb(0 0 0 / 25%)";
-  ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = "rgb(0 0 0 / 25%)";
+    ctx.fillRect(0, 0, width, height);
 
-  for (const ball of balls) {
-    ball.draw();
-    ball.update();
-  }
+    for (const ball of balls) {
+        ball.draw();
+        ball.update();
+        ball.collisionDetect();
+    }
 
-  requestAnimationFrame(loop);
+    requestAnimationFrame(loop);
 }
 
+/* Runs Program */
 loop();
