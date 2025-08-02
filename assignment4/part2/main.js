2f588b4c324d61da0620d380f5d00625f35b2293
RascallyGoose49 <noah.j.chmelnytzki@gmail.com>
2025-08-01 21:18:23 -0400

    Formatting and darkening filter
    Formats some of the comments, and adds the section for darkening the displayed image

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -19,10 +19,13 @@ const imageAlts = {
 /* Looping through images */
 for (let index = 0; index < imageFilenames.length; index++) {
     const newImage = document.createElement('img');
+
+    /* Sets attributes for the source and alt images */
     newImage.setAttribute("src", "images/" + imageFilenames[index]);
     newImage.setAttribute("alt", imageAlts[imageFilenames[index]]);
     thumbBar.appendChild(newImage);
 
+    /* Updates the main image displayed */
     newImage.addEventListener("click", function () {
         displayedImage.setAttribute("src", "images/" + imageFilenames[index]);
         displayedImage.setAttribute("alt", imageAlts[imageFilenames[index]]);
@@ -30,3 +33,24 @@ for (let index = 0; index < imageFilenames.length; index++) {
 }
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', function () {
+
+    // Check the current class on the button
+    const displayedImage = btn.getAttribute('class');
+
+    if (displayedImage == 'dark') {
+        // If class is 'dark', switch to 'light' mode
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+
+        // Applies the darkening filter
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    } else {
+        // If class is not 'dark', switch back to 'dark' mode
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+
+        // Make overlay fully transparent (no darkening)
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
+    }
+});
