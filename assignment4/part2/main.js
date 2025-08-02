f9a87615d924fcfb8abeaa1016cdf705623cb61e
RascallyGoose49 <noah.j.chmelnytzki@gmail.com>
2025-08-01 21:02:22 -0400

    Photo information
    Created the array for the photos; as well as the ALT text for each image.

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -5,8 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const imageAlts = {
+  "pic1.jpg": "Closeup of a blue human eye",
+  "pic2.jpg": "A rock with a unique texture",
+  "pic3.jpg": "White and purple flowers",
+  "pic4.jpg": "An Ancient Egyptian artwork",
+  "pic5.jpg": "A big moth resting on a lead"
+};
 
 /* Looping through images */
 
