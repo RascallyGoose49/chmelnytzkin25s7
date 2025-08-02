8d759d038971bd14a4551a4ded6ae09d4bf40f4d
RascallyGoose49 <noah.j.chmelnytzki@gmail.com>
2025-08-01 21:11:30 -0400

    Updated photo ALT and added loop
    Loop was added, and reformatted most of the document

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -9,18 +9,24 @@ const imageFilenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jp
 
 /* Declaring the alternative text for each image file */
 const imageAlts = {
-  "pic1.jpg": "Closeup of a blue human eye",
-  "pic2.jpg": "A rock with a unique texture",
-  "pic3.jpg": "White and purple flowers",
-  "pic4.jpg": "An Ancient Egyptian artwork",
-  "pic5.jpg": "A big moth resting on a lead"
+    "pic1.jpg": "Closeup of a blue human eye",
+    "pic2.jpg": "A rock with a unique texture",
+    "pic3.jpg": "White and purple flowers",
+    "pic4.jpg": "An Ancient Egyptian artwork",
+    "pic5.jpg": "A big moth resting on a leaf"
 };
 
 /* Looping through images */
+for (let index = 0; index < imageFilenames.length; index++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute("src", "images/" + imageFilenames[index]);
+    newImage.setAttribute("alt", imageAlts[imageFilenames[index]]);
+    thumbBar.appendChild(newImage);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", function () {
+        displayedImage.setAttribute("src", "images/" + imageFilenames[index]);
+        displayedImage.setAttribute("alt", imageAlts[imageFilenames[index]]);
+    });
+}
 
 /* Wiring up the Darken/Lighten button */
